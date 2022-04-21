a20930ad83bf11c69337776a371595f3b8ea3507
seojm0405 <seojm0405@gmail.com>
2022-04-21 16:52:58 +0900

    Initial commit

---
+++ First/src/first/HelloWorld.java
@@ -0,0 +1,10 @@
+package first;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		//기본적인 출력문 (이클립스 sysout cmd+space 로 자동완성)
+		System.out.println("HelloWorld!"); 
+	}
+
+}
