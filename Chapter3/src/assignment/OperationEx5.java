7538bdccc0db20f413c834800a1cabb29c34f72d
seojm0405 <seojm0405@gmail.com>
2022-04-22 18:23:43 +0900

    revise operator

---
+++ Chapter3/src/assignment/OperationEx5.java
@@ -0,0 +1,18 @@
+package assignment;
+
+public class OperationEx5 {
+
+	public static void main(String[] args) {
+		
+		int num1 = 5;
+		int num2 = 10;
+		
+		int result = num1 | num2;
+		System.out.println(result);
+		
+		int num3 = 5;
+		System.out.println(num3 << 1); // 비트 연산자로 왼쪽shift 할시 *2하는 것과 같다
+		// 오른쪽으로 밀면 나누 2 
+	}
+
+}
